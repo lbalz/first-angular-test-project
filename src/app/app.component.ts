c93e57672392ef0411a08c0e8729bada5a194a8e
Lukas Balz <lukasbalz.business@gmail.com>
2024-11-21 17:11:00 +0100

    finished simple sakura ramen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainContentComponent } from "./main-content/main-content.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { LandingPageComponent } from "./main-content/landing-page/landingPage.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
